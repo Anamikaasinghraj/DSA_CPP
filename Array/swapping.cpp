7fa2924204e2b56dce055d60a48d92a005abc013
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-03-23 02:24:16 +0530

    Add files via upload

---
+++ Array/swapping.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) 
+                swap(arr[i], arr[j]); 
+        }
+    }
+}
+
+int main() {
+    int arr[] = {78, 67, 39, 31, 96, 78, 0, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sortArray(arr, n);
+
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << " ";
+
+    return 0;
+}
