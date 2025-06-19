387d9e36c48fd8969fc848c12f2bc46ca2aa81b1
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-06-19 11:20:14 +0530

    Add files via upload

---
+++ Tutorials/basics.cpp
@@ -0,0 +1,17 @@
+// variables:- containers for data.
+
+/* creating a variable
+int val = 10
+*/
+
+//identifiers:
+
+#include <iostream>
+using namespace std;
+//Here car identifiers is used to refer to below class
+class car{
+    string brand;
+    string model;
+    int year;
+
+}
