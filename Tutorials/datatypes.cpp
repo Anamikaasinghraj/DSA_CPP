7d2efd865479a14cecaa9adb2b69c94c23469afa
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-06-19 11:40:51 +0530

    Add files via upload

---
+++ Tutorials/datatypes.cpp
@@ -0,0 +1,35 @@
+//Data types: data type specify the type of data that a variable can store.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int var = 10;
+    cout << var;
+    returns 0;
+
+}
+
+/* types:
+1. Basic data types: Built-in or primitive data types that are used to store simple variables.
+example: int, float, double, char etc.
+
+2. Derived data types: array, pointer, reference, function.
+
+3. user-defined data types: custom data types created by the programmer.
+example: class, struct, union, etc.*/
+
+//Data types conversion:
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+ // prnting the size of each data types
+ cout <<"size of int:" << size of (int)
+ << "bytes" << endl;
+ cout << "size of char:" << size
+ 
+ return 0;
+}
